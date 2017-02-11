dd33036820350d11c9c82a98057916b808dfd8ac
juan <juancruz.23@hotmail.com>
2017-02-11 19:50:28 -0300

    api gets company data and company historical price by its company symbol

---
+++ app/controllers/stockHandler.server.js
@@ -0,0 +1,43 @@
+'use strict';
+
+let apiUrl = 'http://dev.markitondemand.com/MODApis/Api/v2';
+let request = require('request');
+
+function stockHandler () {
+  this.getCompanyData = (companySymbol, callback) => {
+    let url = apiUrl + '/Lookup/json?input=' + companySymbol;
+    this.makeRequest(url, callback);
+  },
+
+  this.getHistorical = (companySymbol, days=3650, callback) => {
+    console.log(days);
+    let parameters = {
+      "Normalized": false,
+      "NumberOfDays": days,
+      "DataPeriod": "Day",
+      "Elements":[{
+          "Symbol":companySymbol,
+          "Type":"price",
+          "Params":["ohlc"]
+        },
+      ]
+    };
+    let stringedParams = JSON.stringify(parameters);
+    let urlHistorical = apiUrl + '/InteractiveChart/json?parameters=' + stringedParams;
+    this.makeRequest(urlHistorical, callback);
+  },
+
+  this.makeRequest = (url, callback) => {
+    request.get({
+      url,
+      json: true,
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }, (err, res, body) => {
+      return callback(err, res, body);
+    });
+  }
+}
+
+module.exports = stockHandler;
--- app/routes/businessLogic.js
+++ app/routes/businessLogic.js
@@ -1,113 +1,27 @@
 'use strict';
 
-module.exports = function (app, appEnv) {
+module.exports = (app, appEnv) => {
 
-  let YelpHandler = require(appEnv.path + '/app/controllers/yelpHandler.server.js');
-  let yelpHandler = new YelpHandler();
-  let BarHandler = require(appEnv.path + '/app/controllers/barHandler.server.js');
-  let barHandler = new BarHandler();
-  let UserHandler = require(appEnv.path + '/app/controllers/userHandler.server.js');
-  let userHandler = new UserHandler();
-
-  app.param("barYelpId",  (req, res, next, barYelpId) => {
-
-    console.log("Requested yelp id: ", barYelpId);
-
-    yelpHandler.businessRequest(barYelpId, (err, response, body) => {
-      if (err) next(err);
-      let yelpJson = JSON.parse(body);
-      req.barJson = yelpJson;
-      return next();
-    });
-  });
-
-  app.use(function(req, res, next){
-    if (req.user && req.query.q) {
-      userHandler.updateLastQuery(req.user, req.query.q, () => {
-        next();
-      })
-    } else {
-      next();
-    }
-  });
+  let StockHandler = require(appEnv.path + '/app/controllers/stockHandler.server.js');
+  let stockHandler = new StockHandler();
 
   app.route('/')
-			.get(function (req, res) {
-				res.render(appEnv.path + '/app/views/index.pug');
-			});
-  app.route('/search')
-    .get((req, res) => {
-      let parameters = {
-        location: req.query.q,
-        sort: '2'
-      }
-      yelpHandler.searchRequest(parameters, function(err, response, body){
-        let yelpJson = JSON.parse(body);
-        let out = {
-          query: {
-            text: req.query.q
-          },
-          yelpJson,
-        }
-        barHandler.getUsersGoing(yelpJson, (err, result) => {
-          if (err) {
-            out.usersGoing = [];
-          } else {
-            out.usersGoing = result.reduce( (pre, post) => {
-                pre[post.yelpId] = post.usersGoing;
-                return pre;
-              }, {});
-          }
-          res.render(appEnv.path + '/app/views/results.pug', out);
-        });
+		.get( (req, res) => {
+			res.render(appEnv.path + '/app/views/index.pug');
+		});
+  app.route('/api/stock/get_company/:symbol')
+		.get( (req, res) => {
+      let companySymbol = req.params.symbol;
+      stockHandler.getCompanyData(companySymbol, (err, response, body) => {
+        res.json(body);
       });
-    });
-  app.route('/api/search')
-    .get((req, res) => {
-      let parameters = {
-        location: req.query.q,
-        sort: '2'
-      }
-      console.log(parameters);
-      yelpHandler.searchRequest(parameters, function(err, response, body){
-        let yelpJson = JSON.parse(body);
-        let out = {
-          query: {
-            text: req.query.q
-          },
-          yelpJson,
-        }
-        barHandler.getUsersGoing(yelpJson, (err, result) => {
-          if (err) {
-            out.usersGoing = [];
-          } else {
-            out.usersGoing = result.reduce( (pre, post) => {
-                pre[post.yelpId] = post.usersGoing;
-                return pre;
-              }, {});
-          }
-          res.json(out);
-        });
+		});
+  app.route('/api/stock/get_historical/:symbol')
+		.get( (req, res) => {
+      let companySymbol = req.params.symbol;
+      let days = undefined;
+      stockHandler.getHistorical(companySymbol, days, (err, response, body) => {
+        res.json(body);
       });
-    });
-
-    app.route('/api/bar/:barYelpId')
-      .get((req, res) => {
-        res.json(req.barJson);
-      })
-      .post(appEnv.middleware.isLoggedIn, (req, res) => {
-        let out = {
-          yelpId: req.params.barYelpId
-        };
-        if (req.barJson.error) {
-          out.error = true;
-          out.message = "Error: " + out.error.text;
-          res.json(out);
-        }
-        barHandler.userGoing(out.yelpId, req.user.id, (err, result) => {
-          out.bar = result;
-          res.json(out);
-        });
-      });
-
+		});
 }
