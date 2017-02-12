f7b0d146ba896278fb907256e59a5f0d0637ff95
juan <juancruz.23@hotmail.com>
2017-02-12 20:47:42 -0300

    testing click event on section

--- app/controllers/stockChart.client.js
+++ app/controllers/stockChart.client.js
@@ -44,7 +44,7 @@
     } else if (e.target && e.target.nodeName === "SPAN") {
       companySymbol = e.target.parentNode.parentNode.id;
     }
-    removeCompany(companySymbol);
+    if (companySymbol) sendRemoveCompany(companySymbol);
     e.stopPropagation();
   }
 
